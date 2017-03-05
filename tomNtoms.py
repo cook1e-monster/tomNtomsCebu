362ac0f6f5ae3d5ec962bcc6edfba0250eb33fcb
carlos <carlos.silva@elevenyellow.com>
2017-03-05 19:28:45 +0800

    add prints and refactor toms object

--- tomNtoms.py
+++ tomNtoms.py
@@ -1,6 +1,7 @@
 import httplib, urllib2, urllib
 from random import randint
 from utils import random_phone_number, extract_redirect, extract_user_password
+import time
 
 class tomNtoms():
 
@@ -13,11 +14,25 @@ class tomNtoms():
         'Upgrade-Insecure-Requests': 1
     }
 
+    __time_to_sleep = 1800
+    __user = None
+    __password = None
+    __referer = None
 
     def generate_user_password( self ):
+        print 'generating user and password'
+
         phone_number = random_phone_number()
+
+        print "phone number to register is: {}".format(phone_number)
         url = "http://172.17.0.1/free_time.cgi?phone={}&phone_user=1&internal=1".format(phone_number)
 
+        try:
+            conn = urllib2.urlopen(url)
+        except urllib2.HTTPError, e:
+            print e.code
+        except urllib2.URLError, e:
+            print e.args
         conn = urllib2.urlopen(url)
 
         content = conn.read()
@@ -26,22 +41,33 @@ class tomNtoms():
         next_url = 'http://172.17.0.1{}'.format(redirect_url)
         print "url to redirect: " + next_url
 
-        conn = urllib2.urlopen(next_url)
-        content = conn.read()
+        try:
+            urllib2.urlopen(next_url)
+        except urllib2.HTTPError, e:
+            print e.code
+        except urllib2.URLError, e:
+            print e.args
 
-        user, password = extract_user_password(content)
+        user, password = extract_user_password( content )
+
+        self.__user = user
+        self.__password = password
+        self.__referer = next_url
+
         print "user: {} and password: {}".format(user, password)
-        return user, password, next_url
+        return
 
 
-    def send_login( self, user, password, referer ):
+    def send_login( self ):
+        print "sending form to register new number"
+
         headers = self.__headers
-        headers['Referer'] = referer
+        headers['Referer'] = self.__referer
 
 
         params = {
-                "username": user,
-                "password": password
+                "username": self.__user,
+                "password": self.__password
                 }
 
         encode_params = urllib.urlencode(params)
@@ -49,5 +75,23 @@ class tomNtoms():
 
         opener = urllib2.build_opener()
         req = urllib2.Request('http://172.17.0.1/weblogin.cgi', encode_params, headers)
-        res = opener.open(req)
-        print res.read()
+
+        try:
+            opener.open(req)
+        except urllib2.HTTPError, e:
+            print e.code
+        except urllib2.URLError, e:
+            print e.args
+
+        print "Perfect, you have internet now. Enjoy. :)"
+        return
+
+    def connect(self):
+        while( True ):
+            print "connecting ...."
+            self.generate_user_password()
+            self.send_login()
+
+
+            print "next connection in {}".format(self.__time_to_sleep)
+            time.sleep(self.__time_to_sleep) # delays for 30 mins
